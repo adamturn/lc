e651e82053e53eee32b7bb8f1e21682a6b92c7c9
adamturn <46877989+adamturn@users.noreply.github.com>
2020-01-28 00:17:10 -0500

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -20,7 +20,6 @@ class Solution:
 			for char in str(x)[1:]:
 			rx = char + rx
 			rx = '-' + rx
-
 		if -2**31 <= int(rx) <= (2**31 - 1):
 			return int(rx)
 		else:
@@ -29,7 +28,6 @@ class Solution:
 	def compact_rvs(self, x: int) -> int:
 		"""Walrus Time!"""
 		(rx := str(x)[::-1]) if x >= 0 else (rx := str(x)[0] + str(x)[1:][::-1])
-
 		if -2**31 <= int(rx) <= 2**31 - 1:
 			return int(rx)
 		else:
